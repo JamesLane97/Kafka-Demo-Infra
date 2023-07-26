ab98d0d7ec3347576233e4b36d2344c338d2c6e1
James Lane <jameslane1997@live.co.uk>
2023-07-26 05:37:28 +0100

    Added Key Vault resource

--- main.tf
+++ main.tf
@@ -26,6 +26,45 @@ resource "azurerm_subnet" "management-subnet" {
   address_prefixes     = ["10.0.2.0/25"]
 }
 
+# 
+data "azurerm_client_config" "current" {}
+
+# Creates the key vault.
+resource "azurerm_key_vault" "key-vault" {
+  name = join("-", [var.project-name, "key-vault"])
+  location = var.deployment-location
+  resource_group_name = azurerm_resource_group.project-resource-group.name
+  enabled_for_disk_encryption = false
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days = 7
+  purge_protection_enabled = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+
+  network_acls {
+    default_action = "Allow"
+    bypass = "AzureServices"
+  }
+}
+
+
 # Creates a NSG with inbound rules for the management subnet.
 module "management-nsg" {
   source            = "./modules/nsg"
