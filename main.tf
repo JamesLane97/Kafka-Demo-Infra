1726116897ae56e07ff01062bd6003786a2dbba2
James Lane <jameslane1997@live.co.uk>
2023-07-30 02:02:18 +0100

    Added ACI resource with a single container.

--- main.tf
+++ main.tf
@@ -31,14 +31,14 @@ data "azurerm_client_config" "current" {}
 
 # Creates the key vault.
 resource "azurerm_key_vault" "key-vault" {
-  name = join("-", [var.project-name, "key-vault"])
-  location = var.deployment-location
-  resource_group_name = azurerm_resource_group.project-resource-group.name
+  name                        = join("-", [var.project-name, "key-vault"])
+  location                    = var.deployment-location
+  resource_group_name         = azurerm_resource_group.project-resource-group.name
   enabled_for_disk_encryption = false
-  tenant_id = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days = 7
-  purge_protection_enabled = false
-  enable_rbac_authorization = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  enable_rbac_authorization   = true
 
   sku_name = "standard"
 
@@ -63,7 +63,7 @@ resource "azurerm_key_vault" "key-vault" {
 
   network_acls {
     default_action = "Allow"
-    bypass = "AzureServices"
+    bypass         = "AzureServices"
   }
 }
 
@@ -79,14 +79,14 @@ module "management-nsg" {
 # Generates the SSH keys for the managment VM.
 resource "tls_private_key" "key-pair" {
   algorithm = "RSA"
-  rsa_bits = 4096
+  rsa_bits  = 4096
 }
 
 # Storing the management VMs private SSH key in the key vault.
 resource "azurerm_key_vault_secret" "managment-vm-private-key" {
-  name = join("-", [var.project-name, "management-vm-PK"])
-  value = tls_private_key.key-pair.private_key_openssh
-  key_vault_id = azurerm_key_vault.key-vault.id 
+  name         = join("-", [var.project-name, "management-vm-PK"])
+  value        = tls_private_key.key-pair.private_key_openssh
+  key_vault_id = azurerm_key_vault.key-vault.id
 }
 
 # Creates the management VM and attached NIC.
@@ -100,3 +100,23 @@ module "management-vm" {
   subnet-id       = azurerm_subnet.management-subnet.id
 }
 
+# Creates the container group along with the instances for the project.
+resource "azurerm_container_group" "container-group" {
+  name                = join("-", [var.project-name, "container-group"])
+  resource_group_name = azurerm_resource_group.project-resource-group.name
+  location            = var.deployment-location
+  ip_address_type     = "Public"
+  os_type             = "Linux"
+
+  container {
+    name   = "kafka"
+    image  = "mcr.microsoft.com/azuredocs/aci-helloworld"
+    cpu    = 0.5
+    memory = 1.5
+
+    ports {
+      port     = 443
+      protocol = "TCP"
+    }
+  }
+}
