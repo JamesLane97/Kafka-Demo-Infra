7ef8093362223239f3af7e387efce30ccb591b9a
James Lane <jameslane1997@live.co.uk>
2023-08-24 12:17:34 +0100

    Added password secret dependency to MySQL Server

--- main.tf
+++ main.tf
@@ -203,6 +203,7 @@ resource "azurerm_mysql_server" "mysql-server" {
   public_network_access_enabled     = false
   ssl_enforcement_enabled           = true
   ssl_minimal_tls_version_enforced  = "TLS1_2"
+  depends_on = [ azurerm_key_vault_secret.mysql-password ]
 }
 
 resource "azurerm_mysql_database" "mysql-database" {
